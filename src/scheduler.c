03d69153fa4c063f913fb5d7587cb570b3142911
Matt Murphy <matt@murphysys.com>
2024-01-29 12:32:46 -0700

    Remove duplicate info from task list

--- src/scheduler.c
+++ src/scheduler.c
@@ -8,27 +8,27 @@
 #include "demo.h"
 
 
-TASK_FN_T tasks[(SIZE_T) N_TASKS] = {
-    &task_demo_tx,          /* TASK_EXEC_DEMO_TX */
-    &task_can_tx,           /* TASK_EXEC_CAN_TX */
-    &task_can_rx,           /* TASK_EXEC_CAN_RX */
-    &task_demo_rx,          /* TASK_EXEC_DEMO_RX */
-    &task_bit_rom,          /* TASK_EXEC_BIT_ROM */
-    &task_empty,            /* TASK_EMPTY5 */
-    &task_empty,            /* TASK_EMPTY6 */
-    &task_empty,            /* TASK_EMPTY7 */
-    &task_empty,            /* TASK_EMPTY8 */
-    &task_empty,            /* TASK_EMPTY9 */
-    &task_empty,            /* TASK_EMPTY10 */
-    &task_empty,            /* TASK_EMPTY11 */
-    &task_empty,            /* TASK_EMPTY12 */
-    &task_empty,            /* TASK_EMPTY13 */
-    &task_empty,            /* TASK_EMPTY14 */
-    &task_empty,            /* TASK_EMPTY15 */
-    &task_empty,            /* TASK_EMPTY16 */
-    &task_empty,            /* TASK_EMPTY17 */
-    &task_empty,            /* TASK_EMPTY18 */
-    &task_empty             /* TASK_EMPTY19 */
+TASK_FN_T tasks[N_TASKS] = {
+    &task_demo_tx,
+    &task_can_tx,
+    &task_can_rx,
+    &task_demo_rx,
+    &task_bit_rom,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty,
+    &task_empty
 };
 
 volatile SIZE_T g_task_idx;
@@ -58,11 +58,11 @@ ISR(TIMER1_OVF_vect)
 
 void scheduler_init(void)
 {
-    timer1_enable(TIMER1_PRESCALE_OVER_256, TIMER1_INTERRUPT_ENABLED);
-    timer1_set(SCHEDULER_TIMER_BASE_VALUE);
-
     g_task_idx = 0;
     g_scheduler_state = SCHEDULER_IDLE;
+    
+    timer1_enable(TIMER1_PRESCALE_OVER_256, TIMER1_INTERRUPT_ENABLED);
+    timer1_set(SCHEDULER_TIMER_BASE_VALUE);
 }
 
 
