287894ea7183fc0537d37d7897e6e320c0a10c67
Matt Murphy <matt@murphysys.com>
2024-02-01 11:47:34 -0700

    Move watchdog code out of high-level file and into hardware file

--- src/atm328p/atm328p_watchdog.h
+++ src/atm328p/atm328p_watchdog.h
@@ -4,20 +4,28 @@
 #include "types.h"
 
 
+/** Watchdog Control and Status Register */
 typedef union {
     struct {
+        /** Watchdog Prescaler bits */
         VBOOL_T WDP0 : 1;
         VBOOL_T WDP1 : 1;
         VBOOL_T WDP2 : 1;
+        /** Watchdog Enable bit */
         VBOOL_T WDE  : 1;
+        /** Watchdog Change Enable bit */
         VBOOL_T WDCE : 1;
+        /** Watchdog Prescaler bit */
         VBOOL_T WDP3 : 1;
+        /** Watchdog Enable Interrupts bit */
         VBOOL_T WDIE : 1;
+        /** Watchdog Interrupt Flag bit */
         VBOOL_T WDIF : 1;
     } bits;
     VU8_T byte;
 } WDTCSR_T;
 
+/** Watchdog Control and Status Register */
 extern volatile WDTCSR_T WDTCSR;
 
 #define WDE_WATCHDOG_DISABLED FALSE
@@ -32,11 +40,16 @@ extern volatile WDTCSR_T WDTCSR;
 #define WDTCSR_WDP2 2
 #define WDTCSR_WDP1 1
 #define WDTCSR_WDP0 0
-/* 256K WDT Oscillator Cycles before Timeout (2.0s at 5V) */
+/** 256K WDT Oscillator Cycles before Timeout (2.0s at 5V) */
 #define WDTCSR_PRESCALE_TIMEOUT_IN_2S \
     (U8_T) ( (1u << WDTCSR_WDP2) | (1u << WDTCSR_WDP1) | (1u << WDTCSR_WDP0) )
 
 #define WDTCSR_ENABLE_WATCHDOG (U8_T) (1u << WDTCSR_WDE)
 #define WDTCSR_CHANGE_WATCHDOG (U8_T) (1u << WDTCSR_WDCE)
+
+
+/** Reset the watchdog timer/counter to 0, preventing watchdog reset. */
+#define watchdog_reset() __asm__ __volatile__ ("wdr")
+
 
 #endif
--- src/watchdog.h
+++ src/watchdog.h
@@ -1,13 +1,9 @@
 #ifndef __WATCHDOG_H__
 #define __WATCHDOG_H__
 
-/** Reset the watchdog timer/counter to 0, preventing watchdog reset. */
-#define watchdog_reset() __asm__ __volatile__ ("wdr")
-
 void watchdog_enable(void);
 void watchdog_disable(void);
 
 void task_watchdog_strobe(void);
-
 
 #endif
